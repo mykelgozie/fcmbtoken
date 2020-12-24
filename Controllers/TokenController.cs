332d7470370df651f175c90f51e4a1121ad5b4a0
michael <mykelgozie@gmail.com>
2020-12-25 00:00:37 +0100

    fcmb

---
+++ Controllers/TokenController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FcmbToken.Dto;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Token;
+using Token.CheckToken;
+
+namespace FcmbToken.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+       
+        private IVerifyToken _verifyToken;
+
+        public TokenController(IVerifyToken verifyToken)
+        {
+            _verifyToken = verifyToken;
+        }
+        
+        [HttpGet]
+        public IActionResult TokenGenerator()
+        {
+
+
+       
+            return Ok(_verifyToken.Verify());
+
+          
+           
+           
+          
+
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Token;
+using Token.CheckToken;
+using Token.TokenGenrator;
+
+namespace FcmbToken
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddScoped<ITokenGenerator, TokenGenerator>();
+            services.AddScoped<IGetToken, GetToken>();
+            services.AddScoped<IVerifyToken, VerifyToken>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
